1ff279e5a8869c32a8ffd288f31a597ee9db09a3
sara <sara.fiorendi@cern.ch>
2015-03-05 12:43:30 +0100

    move input tag label to cfg file

--- test/hltMakeTree_JpsiTrk.py
+++ test/hltMakeTree_JpsiTrk.py
@@ -5,7 +5,7 @@ process = cms.Process("RATE")
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
 #                                 inFile
-		                    "/store/group/phys_bphys/fiorendi/720pre6/FromTemplateMenu_onMC_etaPhi117_addBMass/HLT_DisplacedPaths__phys_bphys_0.root",
+                            "/store/group/phys_bphys/fiorendi/720pre6/FromTemplateMenu_onMC_etaPhi117_addBMass/HLT_DisplacedPaths__phys_bphys_0.root",
                             ),
                             secondaryFileNames = cms.untracked.vstring()  
                             )
@@ -34,38 +34,38 @@ for i in range(len(TRIGGERS)):
   PROCMODU = TRIGGERS[i]+NAMEPLOT[j]
   PROCPATH = 'Path'+PROCMODU
   module =cms.EDAnalyzer("JpsiTrkTreeMaker",
-						 vertexes            = cms.InputTag("offlinePrimaryVertices"),
-						 muons               = cms.InputTag("muons"),
-						 tracks              = cms.InputTag("generalTracks"),
-						 triggerProcess      = cms.string("TRKHLT"),
-						 tagTriggerName      = cms.string("HLT_DoubleMu4_Jpsi_Displaced_v1"),
+                         vertexes            = cms.InputTag("offlinePrimaryVertices"),
+                         muons               = cms.InputTag("muons"),
+                         tracks              = cms.InputTag("generalTracks"),
+                         triggerProcess      = cms.string("TRKHLT"),
+                         tagTriggerName      = cms.string("HLT_DoubleMu4_Jpsi_Displaced_v1"),
                          triggerName         = cms.string(TRIGGERS[i]),
                          probeFilterDen      = cms.string("hltJpsiTkVertexFilter"),#not used
                          probeFilterNum      = cms.string(FILTERS[i]),
-						 maxNumberMuons      = cms.untracked.uint32(999999),
-						 L3CandidatesLabel   = cms.InputTag("hltL3MuonCandidates"),
-						 TrkCandidatesLabel  = cms.InputTag("hltIter2L3MuonMerged"),
-						 VtxLabel            = cms.InputTag("hltOnlinePrimaryVertices"),
-						 MuMuVtxLabel        = cms.InputTag("hltDisplacedmumuVtxProducerDoubleMu4Jpsi"),
+                         maxNumberMuons      = cms.untracked.uint32(999999),
+                         L3CandidatesLabel   = cms.InputTag("hltL3MuonCandidates"),
+                         TrkCandidatesLabel  = cms.InputTag("hltIter2L3MuonMerged"),
+                         VtxLabel            = cms.InputTag("hltOnlinePrimaryVertices"),
+                         MuMuVtxLabel        = cms.InputTag("hltDisplacedmumuVtxProducerDoubleMu4Jpsi"),
                          MuMuVtxProdLabel    = cms.InputTag("hltJpsiTkVertexProducer"),
-						 maxEta              = cms.untracked.double(2.2),
-						 minPtTrk            = cms.untracked.double(0.5),
-						 thirdTrkMass        = cms.untracked.double(0.493677),# kaon
-						 fourthTrkMass       = cms.untracked.double(0.139570),# pion
-						 minInvMass          = cms.untracked.double(4.),
-						 maxInvMass          = cms.untracked.double(7.),
-						 minTrkTrkMass       = cms.untracked.double(0.692),
-						 maxTrkTrkMass       = cms.untracked.double(1.092),
-						 minCandPt           = cms.untracked.double(0),
-						 minCandLxy          = cms.untracked.double(0),
-						 minCandCos          = cms.untracked.double(-1),
-						 maxNormChi2         = cms.untracked.double(10.),
-						 minVtxProb          = cms.untracked.double(0.005),
-						 mind0Sign           = cms.untracked.double(0.),
-						 DisplacedJpsiRequirements = cms.untracked.bool(True),
-						 Debug               = cms.untracked.bool(False),
-						 beamspot            = cms.untracked.InputTag("offlineBeamSpot")
-						 )
+                         maxEta              = cms.untracked.double(2.2),
+                         minPtTrk            = cms.untracked.double(0.5),
+                         thirdTrkMass        = cms.untracked.double(0.493677),# kaon
+                         fourthTrkMass       = cms.untracked.double(0.139570),# pion
+                         minInvMass          = cms.untracked.double(4.),
+                         maxInvMass          = cms.untracked.double(7.),
+                         minTrkTrkMass       = cms.untracked.double(0.692),
+                         maxTrkTrkMass       = cms.untracked.double(1.092),
+                         minCandPt           = cms.untracked.double(0),
+                         minCandLxy          = cms.untracked.double(0),
+                         minCandCos          = cms.untracked.double(-1),
+                         maxNormChi2         = cms.untracked.double(10.),
+                         minVtxProb          = cms.untracked.double(0.005),
+                         mind0Sign           = cms.untracked.double(0.),
+                         DisplacedJpsiRequirements = cms.untracked.bool(True),
+                         Debug               = cms.untracked.bool(False),
+                         beamspot            = cms.untracked.InputTag("offlineBeamSpot")
+                        )
   setattr(process, PROCMODU, module)
   setattr(process, PROCPATH, cms.Path(module))
   process.mypath  = cms.Path(getattr(process,PROCMODU))
